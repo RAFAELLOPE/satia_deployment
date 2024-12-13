204028afb495e0699152c020612d62dd59e0fb78
r.lopez <r.lopez@htime.org>
2024-12-13 17:58:22 +0100

    Resample and filter data

--- src/deploy_model.py
+++ src/deploy_model.py
@@ -59,6 +59,9 @@ def get_meto_data(lat:str,
                  'wind.angle',
                  'cloud_cover.total',
                  'precipitation.total']]
+
+        df['date'] = df['date'].apply(lambda x: datetime.strptime(x, "%Y-%m-%dT%H:%M:%S"))
+        df.set_index('date', inplace=True)
         return df
     except requests.exceptions.RequestException as e:
         raise e
@@ -103,14 +106,42 @@ def process_inverterdatas(inverterdatas:dict) -> pd.DataFrame:
     df = pd.DataFrame(dev_data)
     if not df.empty:
         df.fillna(0, inplace=True)
-        df.set_index('date', inplace=True)
-        df.to_csv(os.path.abspath(os.path.join(os.getcwd(), 'data/inverterdatas.csv')))
     return df
 
 def process_data2inference(df_inv: pd.DataFrame, 
                            df_meteo: pd.DataFrame) -> pd.DataFrame:
-    pass
+    df_inv_c = df_inv.copy()
+    df_meteo_c = df_meteo.copy()
+    df_meteo_c = df_meteo_c.resample('5min').mean().interpolate(method='cubic')
+    df_inv_c = df_inv_c.resample('5min').mean()
+    df_r = df_meteo_c.join(df_inv_c, how='left')
+    df_r.fillna(0, inplace=True)
+    cols = [c.replace('.', '_') for c in df_r.columns]
+    df_r.columns = cols
+    df_r = df_r[['active_power', 
+                 'dev_temperature', 
+                 'ac_voltage', 
+                 'dc_voltage',
+                 'temperature', 
+                 'pressure', 
+                 'cape', 
+                 'irradiance', 
+                 'humidity',
+                 'wind_speed', 
+                 'wind_angle', 
+                 'cloud_cover_total', 
+                 'precipitation_total']]
+    # Save to see how output looks like
+    return df_inv_c, df_meteo, df_meteo_c, df_r
 
+
+def filter_data(df:pd.DataFrame, 
+                var:pd.DataFrame,
+                wind:int = 5) -> pd.DataFrame:
+    df_c = df.copy()
+    df_c[var] = df_c[var].rolling(wind, center=True).mean()
+    df_c[var].fillna(0, inplace=True)
+    return df_c
 
 # This is added so that many files can reuse the function get_database()
 if __name__ == "__main__":    
@@ -121,6 +152,8 @@ if __name__ == "__main__":
 
    start_date = datetime.today().replace(year=2024, month=8, day=25, minute=0, hour=0, second=0)  # Start date (inclusive)
    end_date = start_date + timedelta(days=1)  # End date (inclusive)
+   tomorrow = datetime.today() + timedelta(days=1)
+
    inverter = ObjectId("673f92e7cf2a88fc6f8d53be")
    lon = "3.8488378294343217W"
    lat = "37.35199875174232N"
@@ -134,5 +167,22 @@ if __name__ == "__main__":
    print(query)
    item_details = database.inverterdatas.find(query)
    df_inv = process_inverterdatas(item_details)
+   df_inv['date'] = df_inv['date'].apply(lambda x: x.replace(month = tomorrow.month, day = tomorrow.day))
+   df_inv.set_index('date', inplace=True)
    df_meteo = get_meto_data(lat, lon)
-   process_data2inference(df_inv, df_meteo)
+   df_inv_c, df_meteo, df_meteo_c, df_r = process_data2inference(df_inv, df_meteo)
+   
+   start_filter_date = tomorrow.replace(hour=0, minute=0, second=0)
+   end_filter_date = tomorrow.replace(day=tomorrow.day + 1, hour=0, minute=0, second=0)
+   
+   df_r = df_r.loc[start_filter_date:end_filter_date]
+   
+    
+   df_r = filter_data(df_r, 'active_power', 30)
+   df_r = filter_data(df_r, 'ac_voltage', 30)
+
+#    df_inv_c.to_csv(os.path.abspath(os.path.join(os.getcwd(), 'data/inverter_resampled.csv')))
+#    df_meteo.to_csv(os.path.abspath(os.path.join(os.getcwd(), 'data/meteodatas.csv')))
+#    df_meteo_c.to_csv(os.path.abspath(os.path.join(os.getcwd(), 'data/meteo_resampled.csv')))
+#    df_r.to_csv(os.path.abspath(os.path.join(os.getcwd(), 'data/joined.csv')))
+
